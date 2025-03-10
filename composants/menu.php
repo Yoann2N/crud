fce412bf873844864a9432798e4cbc06fb3419f2
yoann <yoanngervais.yg13750@gmail.com>
2025-03-10 16:11:17 +0100

    ok

---
+++ composants/menu.php
@@ -0,0 +1,7 @@
+<div> 
+    <ul class = "menu"> 
+        <li><a href="index.php">Accueil</a></li> 
+        <li><a href="etudiants.php">Etudiants</a></li> 
+        <li><a href="Cours.php">Cours</a></li> 
+    </ul>
+</div>
--- etudiants.php
+++ etudiants.php
@@ -7,13 +7,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>Etudiants</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
+<?php
         require_once 'composants/menu.php';
     ?>
-    <h1>Bienvenue sur le CRUD</h1>
+    <h1>Etudiants</h1>
 </body>
 </html>
