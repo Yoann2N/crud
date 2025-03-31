839e49ec38e75962fffb582862b2a8294a066947
yoann <yoanngervais.yg13750@gmail.com>
2025-03-31 09:39:27 +0200

    ok

--- views/Layouts/layout_app.php
+++ views/Layouts/layout_app.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$titre?></title>
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <?php
-        require_once '../../composants/menu.php';
+        require_once '../composants/menu.php';
     ?>
     <?=$contenu?>
 </body>
